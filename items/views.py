1aba15483676b0adf796cd1bb98470848a9db6d5
Alexey Vorsoba <106890433+Vorsoba@users.noreply.github.com>
2023-07-01 15:11:16 +0200

    Update views.py

--- items/views.py
+++ items/views.py
@@ -7,8 +7,3 @@ def items_detail(request, id):
     item = get_object_or_404(Item, id=id)
     item_form = ItemForm(instance=item)
     return render(request, 'items_detail.html', {'item': item, 'item_form': item_form})
-
-
-'''def items_detail(request, id):
-    item = get_object_or_404(Item, id=id)
-    return render(request, 'items_detail.html', {'item': item})'''
